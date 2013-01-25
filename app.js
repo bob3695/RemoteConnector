d6e19f047f39cf2c363a5ce1765ef13c568ff5e7
Richard Evans <bob3695@gmail.com>
2013-01-25 11:29:38 -0500

    #RC-1 Fixed

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.get('/api/xbmc/plexbmc/play/:serverIp/:playerIp/:metadataId', xbmcApi.playPl
 app.get('*', routes.index);
 
 // Start server
-
 app.listen(3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
